60d943eb694e60006048cf307797f72920f38a71
SergeyUSB <q1122333@yandex.ru>
2021-06-27 12:58:35 +0700

    start lesson 3

---
+++ src/main/java/com/example/time_serv/TimeServApplication.java
@@ -0,0 +1,13 @@
+package com.example.time_serv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeServApplication.class, args);
+	}
+
+}
