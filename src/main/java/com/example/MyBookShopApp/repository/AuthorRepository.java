4f20e402459c5fe100ae44638d14b2fa6048f094
SergeyUSB <q1122333@yandex.ru>
2021-07-01 07:58:48 +0700

    added author mapper

--- src/main/java/com/example/MyBookShopApp/repository/AuthorRepository.java
+++ src/main/java/com/example/MyBookShopApp/repository/AuthorRepository.java
@@ -1,29 +1,28 @@
 package com.example.MyBookShopApp.repository;
 
 import com.example.MyBookShopApp.dto.Author;
+import com.example.MyBookShopApp.repository.mapper.AuthorMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class AuthorRepository {
     private JdbcTemplate jdbcTemplate;
+    private AuthorMapper authorMapper;
 
     @Autowired
-    public AuthorRepository(JdbcTemplate jdbcTemplate) {
+    public AuthorRepository(JdbcTemplate jdbcTemplate,
+                            AuthorMapper authorMapper) {
         this.jdbcTemplate = jdbcTemplate;
+        this.authorMapper = authorMapper;
     }
+
     public List<Author> getAuthor() {
-        List<Author> authors = jdbcTemplate.query("SELECT * FROM author ORDER BY author ASC", (ResultSet rs, int rowNum) -> {
-            Author author = new Author();
-            author.setId(rs.getInt("id"));
-            author.setAuthor(rs.getString("author"));
-            return author;
-        });
+        List<Author> authors = jdbcTemplate.query("SELECT * FROM author ORDER BY author ASC", authorMapper);
 
         return new ArrayList<>(authors);
     }
